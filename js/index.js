4946f13030334be51811e46a0be424b893437a0f
xyc <739759506@qq.com>
2017-11-28 22:48:37 +0800

    new idear

--- js/index.js
+++ js/index.js
@@ -1,106 +1,113 @@
 window.addEventListener('load', function() {
-	var oWrapper = document.getElementById('wrapper');
-	var json = {
-		imgDescription: ['aaaaaaa', 'bbbbbbbb', 'cccccc', 'dddddddd'],
-		imgData: ['image/1.png','image/2.png','image/3.png','image/4.png']
-	};
+	addImgSlide();
+	function addImgSlide() {
+		var aImg = document.getElementsByTagName('img');
+		var len = aImg.length;
+		// var arr = [];
+		// for(var i = 0; i < len; i++) {
+		// 	if (aImg[i].getAttribute('data-imggroup')){
+		// 		arr.push(aImg[i]);
+		// 	}
+		// }
+		// var json = {};
+		// var newLen = arr.length;
+		// for (var j = 0; j < newLen; j++) {
+		// 	if (!json[arr[j].getAttribute('data-imggroup')]) {
+		// 		json[arr[j].getAttribute('data-imggroup')] = [];
+		// 		json[arr[j].getAttribute('data-imggroup')].push(arr[j]);
+		// 	} else {
+		// 		json[arr[j].getAttribute('data-imggroup')].push(arr[j]);
+		// 	}
+		// }
+		var json = {};
+		for (var i = 0; i < len; i++) {
+			if (aImg[i].getAttribute('data-imggroup')) {
+				if (!json[aImg[i].getAttribute('data-imggroup')]) {
+					json[aImg[i].getAttribute('data-imggroup')] = [];
+					json[aImg[i].getAttribute('data-imggroup')].push(aImg[i]);
+				} else {
+					json[aImg[i].getAttribute('data-imggroup')].push(aImg[i]);
+				}
+			}
+		}
+		console.log(json);
+		var frame = new PowerPoint(json);
+		
+	}
 
-	var frame = new PowerPoint(oWrapper, json);
 
 }, false);
 
-function PowerPoint(obj, opt) {		//参数：包裹元素，包含图片信息的对象
-		this.wrapper = obj;			
-		this.options = opt;
-		this.init();
+function PowerPoint(json) {
+	this.opts = json;
+	this.init();
+}
+PowerPoint.prototype.init = function() {
+	this.addEvent();
+}
+
+PowerPoint.prototype.addEvent = function() {
+	var _this = this;
+	for (var k in _this.opts) {
+		(function(k) {
+			var len = _this.opts[k].length;
+
+			for(var i = 0; i < len; i++) {
+				(function(i) {
+					_this.opts[k][i].addEventListener('click', function() {
+						_this.createFrame(this);
+
+						_this.oClose.addEventListener('click', function() {	//关闭
+							_this.frameClose();
+						}, false);
+
+						var imgMessage = this.getAttribute('alt');
+						_this.addDescription(imgMessage, i + 1, len);
+					}, false);
+				})(i);
+			}
+		})(k);
 	}
 
-	PowerPoint.prototype.init = function() {	//初始化方法
-		this.createNode();		//生成元素
-		this.addEvent();		//添加事件
-	};
 
-	PowerPoint.prototype.createNode = function() {	//生成元素
-		this.imgMain = document.createElement('div');	//创建一个包含图片的div
-		this.len = this.options.imgData.length;			//图片个数
-		var oImg;
-		for (var i = 0; i< this.len; i++) {
-			oImg = document.createElement('img');		//创建图片
-			oImg.setAttribute('src', this.options.imgData[i]);
-			oImg.setAttribute('index', i);	//创建索引
-			this.imgMain.appendChild(oImg); //添加到div中
-		}
-		this.imgMain.setAttribute('class', 'imgmain');	//添加class用于样式
-		this.wrapper.appendChild(this.imgMain);		//div添加到包裹元素中
-	};
+};
 
-	PowerPoint.prototype.addEvent = function() {
-		var _this = this;
-		this.imgMain.addEventListener('click', function(event) {	//包含图片div绑定事件
-			var target = event.target;								//事件目标
-			if (target.nodeName.toLowerCase() === 'img') {			//判断目标类型
-				_this.createFrame(target);							//打开详情
+PowerPoint.prototype.createFrame = function(obj) {	//创建图片详情方法
+	this.oScreen = document.createElement('div');	//创建屏幕遮罩
+	this.oFrame = document.createElement('div');	//创建显示框
+	this.oPrev = document.createElement('button');	//创建上一张及下一张按钮
+	this.oNext = document.createElement('button');	
+	this.oClose = document.createElement('a');		//创建关闭按钮
+	this.pageNum = document.createElement('p');		//页码信息
+	this.imgMessage = document.createElement('p');	//图片描述信息
+	var newObj = obj.cloneNode(true);				//复制传入的对象
 
-				document.addEventListener('click', function(event) {
-					if (_this.oFrame) {
-						var newTarget = event.target;
-						var targetType = newTarget.nodeName.toLowerCase();
-						
-						if (targetType !== 'img' && targetType !== 'p') {
-							_this.frameClose();
-						}
-						// var mouseX = event.clientX,
-						// 	mouseY = event.clientY;
-						// if (mouseX < _this.oFrame.offsetLeft || mouseX > (_this.oFrame.offsetLeft + _this.oFrame.offsetWidth)) {
-						// 	_this.frameClose();
-						// }
-					}
-					
-				}, false);
+	this.oScreen.setAttribute('class', 'screen');	//设置各个的class，用于css
+	this.oFrame.setAttribute('class', 'frame');
+	this.oClose.setAttribute('class', 'close');
+	this.imgMessage.setAttribute('class', 'message');
+	this.pageNum.setAttribute('class', 'pagenum');
 
-				_this.oClose.addEventListener('click', function() {	//关闭
-					_this.frameClose();
-				}, false);
+	this.oClose.innerHTML = '×';					//创建的按钮及信息添加到显示框中
+	this.oFrame.appendChild(this.imgMessage);
+	this.oFrame.appendChild(this.pageNum);
+	this.oFrame.appendChild(newObj);
+	this.oFrame.appendChild(this.oClose);
 
-				var num = parseInt(target.getAttribute('index'));	//获取目标索引
-				_this.addDescription(_this.options.imgDescription[num], num + 1);	//添加描述及页码
-			}
-		}, false);
-	
-	};
+	document.body.appendChild(this.oFrame);			//将显示框添加到body
+	document.body.appendChild(this.oScreen);		//屏幕遮罩添加到body
+};
 
-	PowerPoint.prototype.createFrame = function(obj) {	//创建图片详情方法
-		this.oScreen = document.createElement('div');	//创建屏幕遮罩
-		this.oFrame = document.createElement('div');	//创建显示框
-		this.oPrev = document.createElement('button');	//创建上一张及下一张按钮
-		this.oNext = document.createElement('button');	
-		this.oClose = document.createElement('a');		//创建关闭按钮
-		this.pageNum = document.createElement('p');		//页码信息
-		this.imgMessage = document.createElement('p');	//图片描述信息
-		var newObj = obj.cloneNode(true);				//复制传入的对象
+PowerPoint.prototype.frameClose = function() {
+	document.body.removeChild(this.oFrame);
+	document.body.removeChild(this.oScreen);
+};
 
-		this.oScreen.setAttribute('class', 'screen');	//设置各个的class，用于css
-		this.oFrame.setAttribute('class', 'frame');
-		this.oClose.setAttribute('class', 'close');
-		this.imgMessage.setAttribute('class', 'message');
-		this.pageNum.setAttribute('class', 'pagenum');
-
-		this.oClose.innerHTML = '×';					//创建的按钮及信息添加到显示框中
-		this.oFrame.appendChild(this.imgMessage);
-		this.oFrame.appendChild(this.pageNum);
-		this.oFrame.appendChild(newObj);
-		this.oFrame.appendChild(this.oClose);
-
-		document.body.appendChild(this.oFrame);			//将显示框添加到body
-		document.body.appendChild(this.oScreen);		//屏幕遮罩添加到body
-	};	
-
-	PowerPoint.prototype.addDescription = function(str, index) {
+PowerPoint.prototype.addDescription = function(str, index, len) {
 		this.imgMessage.innerHTML = str;						//图片信息添加内容
-		this.pageNum.innerHTML = index + " / " +  this.len;		//页码
-	};
+		this.pageNum.innerHTML = index + " / " +  len;		//页码
+};
 
-	PowerPoint.prototype.frameClose = function() {
-		document.body.removeChild(this.oFrame);
-		document.body.removeChild(this.oScreen);
-	};
+PowerPoint.prototype.go = function(n) {
+
+}
